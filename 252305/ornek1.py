7b54c0116e7454e92612d03eb8f2366f3e4982c6
ecrinberra <ecrinberrakagizman@gmail.com>
2025-05-23 11:38:40 +0300

    Add files via upload

---
+++ 252305/ornek1.py
@@ -0,0 +1,5 @@
+kisiler=[]
+for kisi in range(5):
+    kisiler.append(input("arkadaşınızın adını girin:"))
+for kisi in kisiler:
+      print("my best friend:",kisi)
---
+++ 252305/ornek2.py
@@ -0,0 +1,5 @@
+ders=[]
+for i in range (6):
+    ders.append(input("dersin adını giriniziz:"))
+for ders_not in ders :
+    print ("ders notları",ders_not)
